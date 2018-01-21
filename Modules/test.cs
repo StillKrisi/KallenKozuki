41d5386501769ab6c58bdd7cd2c56acf200725e2
StillKrisi <35642801+StillKrisi@users.noreply.github.com>
2018-01-21 15:38:17 +0100

    Initial commit

---
+++ Modules/test.cs
@@ -0,0 +1,43 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kallen.Modules
+{
+    public class test : ModuleBase<SocketCommandContext>
+
+    {
+
+        [Command("Kallen")]
+        public async Task Kallen()
+        {
+
+            await Context.Channel.SendMessageAsync("!We judge ourselves by what we feel capable of doing, while others judge us by what we have already done");
+        }
+
+        [Command("Motto")]
+        public async Task Motto()
+        {
+
+            await Context.Channel.SendMessageAsync("!The road we must walk is uphill ... That's why we are destined to go high!");
+        }
+
+        [Command("anchorman")]
+        public async Task anchorman()
+        {
+
+            await Context.Channel.SendMessageAsync("!**Vivi con classe Nomen Vision!!**");
+        }
+
+        [Command("shepard")]
+        public async Task shepard()
+        {
+
+            await Context.Channel.SendMessageAsync("!Please, dear God, don't let me fuck up.");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace Kallen
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+            new CommandHandler(_client);
+
+            await _client.LoginAsync(TokenType.Bot, "NDA0NjA4NzY0ODM1MjY2NTYw.DUYXZw.i6sjnhDbhW1_jO6gC8zXU4NV5mY");
+
+            await _client.StartAsync();
+
+            _handler = new CommandHandler(_client);
+
+            await Task.Delay(-1);
+        }
+
+    }
+}
